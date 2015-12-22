012722b95b7f2f69983f3803b6d0fcb32babaf9e
AndrKlesh <andr_mast@mail.ru>
2015-12-22 16:16:53 +0600

    Add discover peers

--- src/RadioClub/RadioWiFi/RadioSetWiFiActivity.java
+++ src/RadioClub/RadioWiFi/RadioSetWiFiActivity.java
@@ -1,17 +1,30 @@
 package RadioClub.RadioWiFi;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
 public class RadioSetWiFiActivity extends Activity {
+
     private Button discoverButton;
-    private ListView devicesListView;
-    private String[] testListView;
-    private Button sendButton;
-    private TextView chatTextView;
-    private EditText messageEditText;
+    private final IntentFilter intentFilter = new IntentFilter();
+    private Channel mChannel;
+    private WifiP2pManager mManager;
+    private boolean isWifiP2pEnabled;
+    private WiFiDirectBroadcastReceiver receiver;
+
+    /**
+     * @param isWifiP2pEnabled the isWifiP2pEnabled to set
+     */
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
 
     /**
      * Called when the activity is first created.
@@ -21,37 +34,60 @@ public class RadioSetWiFiActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testListView = new String[]{"Device1", "Device2", "Device3", "Device4"};
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
 
-        devicesListView = (ListView) findViewById(R.id.main_devices_listview);
-        devicesListView.setAdapter(new ArrayAdapter<>(this, R.layout.list_view_item, testListView));
-        devicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                devicesListView.setVisibility(View.GONE);//GONE — элемент невидим и не занимает место в разметке
-                chatTextView.setVisibility(View.VISIBLE);
-                chatTextView.setText(getString(R.string.start_message_in_chat));
-                messageEditText.setVisibility(View.VISIBLE);
-                sendButton.setVisibility(View.VISIBLE);
-            }
-        });
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
         discoverButton = (Button) findViewById(R.id.main_discover_button);
         discoverButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                devicesListView.setVisibility(View.VISIBLE);
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
 
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(RadioSetWiFiActivity.this, "Discovery Initiated",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reasonCode) {
+                        Toast.makeText(RadioSetWiFiActivity.this, "Discovery Failed : " + reasonCode,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
-        chatTextView = (TextView) findViewById(R.id.main_chat_textview);
-        messageEditText = (EditText) findViewById(R.id.main_message_edittext);
-        sendButton = (Button) findViewById(R.id.main_send_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chatTextView.append(messageEditText.getText() + "\n" + getString(R.string.start_message_in_chat));
-            }
-        });
+    }
+
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    public void resetData() {
+
+
     }
 }
